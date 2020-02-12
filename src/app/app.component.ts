f98253c32e5b41d5f89dff5e4cdeb67e80b65963
shoolpani dubey <shool.pani.dubey@gmail.com>
2020-02-12 16:11:47 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+import { faTrash,faCheck, IconDefinition } from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-demo';
+  title:string = 'todo-demo';
+  faTrash:IconDefinition= faTrash;
+  faCheck:IconDefinition= faCheck;
+  addTodoText:string= '';
+  todoList:Array<object>= [];
+
+  addToDo:Function= this.addToDoImpl;
+  deleteToDo:Function= this.deleteToDoImpl;
+  checkToDo:Function= this.checkToDoImpl;
+
+  //Function defination at the bottom, this improves readability and code understanding
+  addToDoImpl(){
+    const toDo={
+      ifDone: false,
+      text: this.addTodoText
+    }
+    this.todoList.unshift(toDo);
+    this.addTodoText="";
+  }
+  deleteToDoImpl(ind:number){
+    this.todoList.splice(ind,1);
+  }
+  checkToDoImpl(ind:number){
+    const checkedToDo= this.todoList[ind];
+    this.todoList.splice(ind,1);
+    checkedToDo['ifDone']=true;
+    this.todoList.push(checkedToDo);
+  }
 }
